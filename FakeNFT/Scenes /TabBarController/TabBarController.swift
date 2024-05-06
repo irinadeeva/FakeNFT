4b6c8a656cb25d75a642f95032b890c5be5ba3b4
irinadeeva <deeva.irina.a@gmail.com>
2024-05-06 09:53:36 +0200

    Revert "added Profile tab"
    This reverts commit 050af8106a1ef4c51dff407b998a4e01bbfa5894.

--- FakeNFT/Scenes /TabBarController/TabBarController.swift
+++ FakeNFT/Scenes /TabBarController/TabBarController.swift
@@ -4,30 +4,21 @@ final class TabBarController: UITabBarController {
 
     var servicesAssembly: ServicesAssembly!
 
-    private let profileTabBarItem = UITabBarItem(
-        title: NSLocalizedString("Tab.profile", comment: ""),
-        image: UIImage(systemName: "person.crop.circle.fill"),
-        tag: 0
-    )
-
     private let catalogTabBarItem = UITabBarItem(
         title: NSLocalizedString("Tab.catalog", comment: ""),
         image: UIImage(systemName: "square.stack.3d.up.fill"),
-        tag: 1
+        tag: 0
     )
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let profileController = ProfileViewController(servicesAssembly: servicesAssembly)
-        profileController.tabBarItem = profileTabBarItem
 
         let catalogController = TestCatalogViewController(
             servicesAssembly: servicesAssembly
         )
         catalogController.tabBarItem = catalogTabBarItem
 
-        viewControllers = [profileController, catalogController]
+        viewControllers = [catalogController]
 
         view.backgroundColor = .systemBackground
     }
